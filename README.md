3fba2ce4b1f5f2ab12aca4cf283a396dea8cb9b1
Lance Bragstad <lbragstad@gmail.com>
2018-04-20 14:31:48 -0500

    Add script for building bug reports

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # launchpad-toolkit
-A set of scripts to help with administrative tasks and management of projects hosted on Launchpad.
+A set of scripts to help with administrative tasks and management of projects
+hosted on Launchpad.
 
 ## Installing Dependencies
 
@@ -14,7 +15,64 @@ $ source .venv/bin/activate
 (.venv)$ pip install -r requirements.txt
 ```
 
+## Building Bug Reports
+
+This script will scrap Launchpad for bugs within a given time window and
+attempt to build a report.
+
+```
+$ python bug_report.py -h
+usage: bug_report.py [-h] [--launchpad-config-file LAUNCHPAD_CONFIG_FILE]
+                     [--report-config-file REPORT_CONFIG_FILE]
+
+Keystone bug reports.
+
+optional arguments:
+  -h, --help            show this help message and exit
+  --launchpad-config-file LAUNCHPAD_CONFIG_FILE
+  --report-config-file REPORT_CONFIG_FILE
+```
+
+Start by putting the report dates and projects in a configuration file:
+
+```
+$ cat << EOF > report.conf
+> [report]
+> projects = keystone keystoneauth keystonemiddleware python-keystoneclient
+> start_date = 'April 13, 2018 12:00 AM CST'
+> end_date = 'April 20, 2018 12:00 AM CST'
+> EOF
+```
+
+Then run the script:
+
+```
+$ python bug_report.py
+Bugs opened (5)
+Bug #1763824 (keystone:Medium) opened by Morgan Fainberg https://bugs.launchpad.net/keystone/+bug/1763824
+Bug #1765193 (keystone:Medium) opened by Lance Bragstad https://bugs.launchpad.net/keystone/+bug/1765193
+Bug #1764272 (keystone:Undecided) opened by lucky https://bugs.launchpad.net/keystone/+bug/1764272
+Bug #1764274 (keystone:Undecided) opened by lucky https://bugs.launchpad.net/keystone/+bug/1764274
+Bug #1764282 (keystone:Undecided) opened by lucky https://bugs.launchpad.net/keystone/+bug/1764282
+
+Bugs closed (0)
+
+Bugs fixed (4)
+Bug #1763824 (keystone:Medium) fixed by Morgan Fainberg https://bugs.launchpad.net/keystone/+bug/1763824
+Bug #1754413 (keystone:Low) fixed by Russell Tweed https://bugs.launchpad.net/keystone/+bug/1754413
+Bug #1754417 (keystone:Low) fixed by Russell Tweed https://bugs.launchpad.net/keystone/+bug/1754417
+Bug #1755874 (keystone:Undecided) fixed by Morgan Fainberg https://bugs.launchpad.net/keystone/+bug/1755874
+```
+
+This script does use a ``.launchpadtk.conf`` file to pull information from
+Launchpad using your account information. A sample one has been included in
+this repository and by default ``bug_report.py`` will look for it in your home
+directory.
+
 ## Recent Bugs
+
+Note that most of this functionality has been superseded by ``bug_report.py``,
+which is easier to use.
 
 A basic script that reports all new bug activity within a given time period.
 
@@ -52,6 +110,9 @@ projects.
 ```
 
 ## Office Hours
+
+Note that most of this functionality has been superseded by ``bug_report.py``,
+which is easier to use.
 
 Generate a report consisting of all bug activity for a project within a given
 time period.
---
+++ tools/bug_report.py
@@ -0,0 +1,117 @@
+import argparse
+import ConfigParser
+
+import dateparser
+
+import os
+
+from launchpadlib import launchpad
+
+LP_INSTANCE = 'production'
+LP_STATUS = (
+    'New', 'Incomplete', 'Opinion', 'Invalid', 'Won\'t Fix', 'Confirmed',
+    'Triaged', 'In Progress', 'Fix Committed', 'Fix Released'
+)
+CLOSED_STATUS = (
+    'Opinion', 'Invalid', 'Won\'t Fix',
+)
+FIXED_STATUS = (
+    'Fix Committed', 'Fix Released'
+)
+
+
+def configure(config_file):
+    config = ConfigParser.SafeConfigParser()
+    config.read(config_file)
+    return config
+
+
+def main(launchpad_config, report_config):
+    lp = launchpad.Launchpad.login_with(
+        launchpad_config.get('launchpad', 'account_name'),
+        LP_INSTANCE,
+        launchpad_config.get('launchpad', 'cache_dir')
+    )
+
+    start_date = report_config.get('report', 'start_date')
+    end_date = report_config.get('report', 'end_date')
+    settings = {'TO_TIMEZONE': 'UTC'}
+    start_datetime = dateparser.parse(start_date, settings=settings)
+    end_datetime = dateparser.parse(end_date, settings=settings)
+
+    created_bugs = []
+    closed_bugs = []
+    fixed_bugs = []
+    for project in report_config.get('report', 'projects').split():
+        bugs = lp.projects[project].searchTasks(
+            status=LP_STATUS, modified_since=start_datetime
+        )
+        for bug in bugs:
+            created_at = bug.date_created
+            if created_at <= end_datetime and created_at >= start_datetime:
+                created_bugs.append(bug)
+
+            if bug.status in CLOSED_STATUS:
+                closed_at = bug.date_closed
+                if closed_at <= end_datetime and closed_at >= start_datetime:
+                    closed_bugs.append(bug)
+
+            if bug.status in FIXED_STATUS:
+                closed_at = bug.date_closed
+                if not closed_at:
+                    closed_at = bug.date_fix_committed
+                if closed_at <= end_datetime and closed_at >= start_datetime:
+                    fixed_bugs.append(bug)
+
+    print('Bugs opened (%d)' % len(created_bugs))
+    for bug in created_bugs:
+        s = 'Bug #%(number)s (%(target)s:%(sev)s) opened by %(owner)s %(link)s'
+        s = s % {
+            'number': bug.bug.id,
+            'target': bug.bug_target_name,
+            'sev': bug.importance,
+            'owner': bug.owner.display_name,
+            'link': bug.web_link
+        }
+        print(s)
+
+    print('\nBugs closed (%d)' % len(closed_bugs))
+    for bug in closed_bugs:
+        s = 'Bug #%(number)s (%(target)s:%(sev)s) %(link)s'
+        s = s % {
+            'number': bug.bug.id,
+            'target': bug.bug_target_name,
+            'sev': bug.importance,
+            'link': bug.web_link
+        }
+        print(s)
+
+    print('\nBugs fixed (%d)' % len(fixed_bugs))
+    for bug in fixed_bugs:
+        assignee = bug.assignee.display_name
+        s = ('Bug #%(number)s (%(target)s:%(sev)s) fixed by '
+             '%(assignee)s %(link)s')
+        s = s % {
+            'number': bug.bug.id,
+            'target': bug.bug_target_name,
+            'sev': bug.importance,
+            'assignee': assignee if assignee else 'no one',
+            'link': bug.web_link
+        }
+        print(s)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description='Keystone bug reports.')
+    parser.add_argument(
+        '--launchpad-config-file', default='~/.launchpadtk.conf'
+    )
+    parser.add_argument('--report-config-file', default='sample.conf')
+    args = parser.parse_args()
+
+    launchpad_config = configure(
+        os.path.expanduser(args.launchpad_config_file)
+    )
+    report_config = configure(args.report_config_file)
+    main(launchpad_config, report_config)
